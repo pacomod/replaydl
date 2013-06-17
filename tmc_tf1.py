97eb80be869b09584ae745a2762d4a2c3938881f
pacome <pacome.odd@gmail.com>
2013-06-17 23:31:34 +0200

    V0.9.1 k3c et bibichouchou: correction hash/size swf

--- tmc_tf1.py
+++ tmc_tf1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-# -*- coding:utf-8 -*-
-# TF1 TMC NT1 HD1 version 0.8 par k3c, telecharge lien en rtmpdump en HD, pas curl
+#-*- coding:utf-8 -*-
+# TF1 TMC NT1 HD1 V0.9.1 k3c et bibichouchou: correction hash/size swf
 import subprocess, optparse, re, sys, shlex
 import socket
 from urllib2 import urlopen
 import time, md5, random, urllib2, json
 import bs4 as BeautifulSoup
+
 listeUserAgents = [ 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; fr-fr) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1',
                                                 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1',
                                                 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.',
@@ -18,16 +19,17 @@ listeUserAgents = [ 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; fr-fr) Ap
 WEBROOTWAT          = "http://www.wat.tv"
 wat_url      = "/web/"
 jsonVideosInfos    = ""
-def get_soup(url, referer):
+ua = random.choice(listeUserAgents)
+
+def get_soup(url, referer, ua):
     """ on récupère la soupe """
-    ua = random.choice(listeUserAgents)
     req  = urllib2.Request(url)
     req.add_header('User-Agent', ua)
     req.add_header('Referer', referer)
     soup = urllib2.urlopen(req).read()
     return soup
 
-def get_wat(id):
+def get_wat(id, HDFlag):
     """la fonction qui permet de retrouver une video sur wat"""
     def base36encode(number):
         if not isinstance(number, (int, long)):
@@ -40,11 +42,17 @@ def get_wat(id):
             number, i = divmod(number, 36)
             base36 = alphabet[i] + base36
         return base36 or alphabet[0]
-    ts = base36encode(int(time.time()))
+
+    if HDFlag:
+        wat_url = "/webhd/"
+    else:
+        wat_url = "/web/"
+
+    ts = base36encode(int(time.time())-60)
     timesec = hex(int(ts, 36))[2:]
     while(len(timesec)<8):
         timesec = "0"+timesec
-    token = md5.new("9b673b13fa4682ed14c3cfa5af5310274b514c4133e9b3a81e6e3aba00912564/web/"+str(id)+""+timesec).hexdigest()
+    token = md5.new("9b673b13fa4682ed14c3cfa5af5310274b514c4133e9b3a81e6e3aba00912564"+wat_url+str(id)+""+timesec).hexdigest()
     id_url1 = WEBROOTWAT+"/get"+wat_url+str(id)+"?token="+token+"/"+str(timesec)+"&country=FR&getURL=1"
     return id_url1
 
@@ -61,7 +69,7 @@ def main():
         parser.print_help()
         parser.exit( 1 )
     debut_id = ''
-    html = urlopen(sys.argv[1]).read()
+    html = urllib2.urlopen(sys.argv[1]).read()
     nom = sys.argv[1].split('/')[-1:][0]
     no = nom.split('.')[-2:][0]
     soup = BeautifulSoup.BeautifulSoup(html)
@@ -71,54 +79,55 @@ def main():
         debut_id = str(soup.find('section', attrs={'class' : 'player-unique' }))
     id = [x.strip() for x in re.findall("mediaId :([^,]*)", debut_id)][0]
     referer = [x.strip() for x in re.findall('url : "(.*?)"', debut_id)][0]
-    id_url1 = get_wat(id)
-    req  = urllib2.Request(id_url1)
-    req.add_header('User-Agent', random.choice(listeUserAgents))
-    req.add_header('Referer', referer)
-    data = urllib2.urlopen(req).read()
-    ua = random.choice(listeUserAgents)
-    jsonVideoInfos = get_soup(WEBROOTWAT+'/interface/contentv3/'+id, referer)
+    jsonVideoInfos = get_soup(WEBROOTWAT+'/interface/contentv3/'+id, referer, ua)
     videoInfos     = json.loads(jsonVideoInfos)
-    try :
-        HD = videoInfos["media"]["files"][0]["hasHD"]
-        data = data.replace('H264-384x288','HD-1280x720').replace('.h264','.hd')
-        data0 = re.search('rtmpte://(.*)hd', data).group(0)
-        wat_url = "/webhd/"
-    except :
-        wat_url = "/web/"  
-        data0 = re.search('rtmpte://(.*)h264', data).group(0)  
-    if data[0:4] == 'http':
-        ua = random.choice(listeUserAgents)
-        arguments = 'curl "%s" -C - -L -g -A "%s" -o "%s.mp4"' % (data, ua, no)
-        print arguments
-        p = subprocess.Popen(arguments, stdout=subprocess.PIPE, shell=True).communicate()[0]
-    if data[0:4] == 'rtmp':
-        host = re.search('rtmpte://(.*)/ondemand', data).group(1)
-        host = host.replace('rtmpte', 'rtmpe')
 
-        data0 = data0.replace('rtmpte','rtmpe')
-        # cmds = 'rtmpdump -r "%s" -c 443 -m 10 -w b23434cbed89c9eaf520373c4c6f26e1f7326896dee4b1719e8d9acda0c19e99 -x 343427 -o "%s.mp4" " --resume"' % (data0, str(no)) # needs a new swf hash ?
-        cmds='rtmpdump -r "%s" -c 1935  -o "%s.mp4" -e' % (data0, str(no))
-        f = open(str(no), 'w')
-        f.write(cmds)
-        f.close()
-        arguments = shlex.split( cmds )
-        print arguments
-        cpt = 0 
-        while True:
-            p = subprocess.Popen( arguments,
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate()
+    try:
+        HD = videoInfos["media"]["files"][0]["hasHD"]
+    except:
+        HD = False
+
+    NumberOfParts = len(videoInfos["media"]["files"])
+    ListOfIds = []
+    for iPart in range(NumberOfParts):
+        ListOfIds.append(videoInfos["media"]["files"][iPart]["id"])
+
+    for PartId in ListOfIds:
+        id_url1 = get_wat(PartId, HD)
+        req  = urllib2.Request(id_url1)
+        req.add_header('User-Agent', ua)
+        req.add_header('Referer', referer)
+        data = urllib2.urlopen(req).read()
+        print data
+        print type(data)
+        if data[0:4] == 'http':
+            arguments = 'curl "%s" -C - -L -g -A "%s" -o "%s.mp4"' % (data, ua, no + "-" + str(PartId))
+            print arguments
+            process = subprocess.Popen(arguments, stdout=subprocess.PIPE, shell=True).communicate()[0]
+        if data[0:4] == 'rtmp':
+            if '.hd' in data:
+                data0 = re.search('rtmpte://(.*)hd', data).group(0)
+            if '.h264' in data:
+                data0 = re.search('rtmpte://(.*)h264', data).group(0)
+            data0 = data0.replace('rtmpte','rtmpe')
+            cmds = 'rtmpdump -e -r "%s" -c 443 -m 10 -w 0818931e9bfa764b9c33e42de6d06f924ac7fc244d0d4941019b9cdfe8706705 -x 352043 -o "%s.mp4"' % (data0, str(no) + "-" + str(PartId))
+            print cmds
+            arguments = shlex.split( cmds )
+            print arguments
+            cpt = 0 
+            while True:
+                p = subprocess.Popen( arguments,
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = p.communicate()
             if p.returncode != 0:
-                print "stdout: " + stdout
-                print "stderr: " + stderr
                 print "Erreur : le sous-process s\'est terminé avec (le code d\'erreur est " + str(p.returncode) + ")"
-#                status = False
                 if cpt > 5:
                     break
                 cpt += 1
                 time.sleep(3) 
             else:
                 break
+
+
 if __name__ == "__main__":
     main()
