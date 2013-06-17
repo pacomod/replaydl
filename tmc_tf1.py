42d894c7fdbcb4fd34d9be5eb9a758692526db4d
pacome <pacome.odd@gmail.com>
2013-06-18 00:30:54 +0200

    V0.9.4: rtmpdump -W, vérif programmes externes, algo conversion

--- tmc_tf1.py
+++ tmc_tf1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-# TF1 TMC NT1 HD1 V0.9.3 par k3c, bibichouchou et pacome: test site avec urlparse
+# TF1 TMC NT1 HD1 V0.9.4 par k3c, bibichouchou et pacome: rtmpdump -W, vérif programme externes, algo conversion 
 
 import subprocess, optparse, re, sys, shlex
 import socket
@@ -11,18 +11,41 @@ import os                       # → os.remove
 from urlparse import urlparse
 
 listeUserAgents = [ 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; fr-fr) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1',
-                                                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1',
-                                                'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.',
-                                                'Mozilla/5.0 (X11; U; Linux x86_64; en-us) AppleWebKit/528.5+ (KHTML, like Gecko, Safari/528.5+) midori',
-                                                'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.107 Safari/535.1',
-                                                'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en-us) AppleWebKit/312.1 (KHTML, like Gecko) Safari/312',
-                                                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
-                                                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.8 (KHTML, like Gecko) Chrome/17.0.940.0 Safari/535.8' ]
+                    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1',
+                    'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.',
+                    'Mozilla/5.0 (X11; U; Linux x86_64; en-us) AppleWebKit/528.5+ (KHTML, like Gecko, Safari/528.5+) midori',
+                    'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.107 Safari/535.1',
+                    'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en-us) AppleWebKit/312.1 (KHTML, like Gecko) Safari/312',
+                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
+                    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.8 (KHTML, like Gecko) Chrome/17.0.940.0 Safari/535.8' ]
 
-WEBROOTWAT          = "http://www.wat.tv"
-wat_url      = "/web/"
-jsonVideosInfos    = ""
-ua = random.choice(listeUserAgents)
+WEBROOTWAT="http://www.wat.tv"
+wat_url="/web/"
+jsonVideosInfos=""
+ua=random.choice(listeUserAgents)
+# swf players
+swfWatPlayerLite="www.wat.tv/images/v40/PlayerWat.swf" # fourni par JUL1EN094
+swfWatPlayerWat="www.wat.tv/images/v70/PlayerLite.swf" # trouvé sur le www.wat.tv
+# programmes externes utilisés
+ffmpegEx='ffmpeg'
+rtmpdumpEx='rtmpdump'
+
+def checkExternalProgram(prog, optArg='', expectedValue=''):
+    """ Permet de vérifier la présence des programmes externes requis """
+    args=shlex.split('%s %s' % (prog, optArg))
+    try:
+        process=subprocess.Popen(args,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = process.communicate()
+        if expectedValue == '':
+            return True
+        else:
+            if expectedValue in stdout: # à améliorer pour versions > ...
+                return True
+            else:
+                return False
+    except OSError:
+        print 'Le programme %s n\'est pas présent sur votre système' % (prog)
+        return False
 
 def get_soup(url, referer, ua):
     """ on récupère la soupe """
@@ -59,6 +82,11 @@ def get_wat(id, HDFlag):
 
 def main():
     """ recuperation de vidéos sur TF1/TMC/NT1/HD1 (donc WAT)"""
+    # vérification de la présence de rtmpdump v2.4 ou v2.5
+    if not checkExternalProgram(rtmpdumpEx, '-help', 'v2.4'):
+        if not checkExternalProgram(rtmpdumpEx, '-help', 'v2.5'): # pas top...
+            print 'Ce script requiert %s v2.4 ou v2.5' % (rtmpdumpEx)
+            return
     # timeout en secondes
     socket.setdefaulttimeout(90)
     usage   = "usage: python tmc_tf1.py     [options] <url de l'emission>"
@@ -113,14 +141,22 @@ def main():
                 data0 = re.search('rtmpte://(.*)h264', data).group(0)
             data0 = data0.replace('rtmpte','rtmpe')
             fName=str(no) + "-" + str(PartId) # nom du fichier final sans extension
-            cmds = 'rtmpdump -e -r "%s" -c 443 -m 10 -w 0818931e9bfa764b9c33e42de6d06f924ac7fc244d0d4941019b9cdfe8706705 -x 352043 -o "%s.t.mp4"' % (data0, fName)
-            print cmds
-            arguments = shlex.split( cmds )
+            fileName=fName+".mp4"             # nom du fichier final avec extension
+            # ffmpeg est-il disponible?
+            if not checkExternalProgram(ffmpegEx):
+                print "L'installation de ffmpeg sur votre système permettrait de corriger automatiquement le conteneur de la vidéo (flash→mp4)."
+                tmpFileName=fileName
+                ffmpegAvailable=False
+            else:
+                tmpFileName=fName+".tmp.mp4"
+                ffmpegAvailable=True
+            rtmpCmd = '%s -e -r "%s" -c 443 -m 10 -W %s -o "%s"' % (rtmpdumpEx, data0, swfWatPlayerLite, tmpFileName)
+            print rtmpCmd
+            arguments = shlex.split( rtmpCmd )
             # print arguments
             cpt = 0 
             while True:
-                p = subprocess.Popen( arguments,
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                p = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 stdout, stderr = p.communicate()
                 if p.returncode != 0:
                     print "Erreur : le sous-process s\'est terminé avec (le code d\'erreur est " + str(p.returncode) + ")"
@@ -129,16 +165,17 @@ def main():
                     cpt += 1
                     time.sleep(3) 
                 else:
-                    # conversion ffmpeg fName.t.mp4 → fName.mp4 (pour corriger le conteneur)
-                    cmdFfmpeg='ffmpeg -i "%s.t.mp4" -acodec copy -vcodec copy "%s.mp4"' % (fName, fName)
-                    arguments=shlex.split(cmdFfmpeg)
-                    p=subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    stdout, stderr = p.communicate()
-                    if p.returncode != 0:
-                        print 'Erreur: la conversion ffmpeg s\'est terminée avec le code d\'erreur %i.\nLe fichier %s.t.mp4 est néanmois disponible' % (p.returncode, fName)
-                    else:
-                        # suppression du fichier temporaire
-                        os.remove(fName+'.t.mp4')
+                    if ffmpegAvailable:
+                        # conversion ffmpeg tmpFileName → fileName (pour corriger le conteneur)
+                        ffmpegCmd='ffmpeg -i "%s" -acodec copy -vcodec copy "%s"' % (tmpFileName, fileName)
+                        arguments=shlex.split(ffmpegCmd)
+                        p=subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        stdout, stderr = p.communicate()
+                        if p.returncode != 0:
+                            print 'Erreur: la conversion ffmpeg s\'est terminée avec le code d\'erreur %i.\nLe fichier %s est néanmois disponible' % (p.returncode, tmpFileName)
+                        else:
+                            # suppression du fichier temporaire
+                            os.remove(tmpFileName)
                     break
 
 if __name__ == "__main__":
